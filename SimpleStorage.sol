ce28882727ebfc06fb5176089ef0eee51380f0da
Filipe Ines <filipe.jl.ines@gmail.com>
2023-05-12 19:51:55 +0100

    add title and description to items

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -7,25 +7,27 @@ contract SimpleStorage {
     struct item { 
         int price;
         address  owner;
-        int buyer;
+        string buyer;
         bool paid;
+        string title;
+        string description;
     }
 
     int[] itemsArray;
     mapping(uint => item) public items;
 
-    function storeItem(int price, address owner) public {
+    function storeItem(int price, address owner, string memory title, string memory description) public {
         uint itemID = itemsArray.length;
         itemsArray.push(int(itemsArray.length));
-        items[itemID] = item({price:price, owner:owner, paid:false, buyer:0});
+        items[itemID] = item({price:price, owner:owner, paid:false, buyer:"", title: title, description: description});
     }
 
-    function getItem(uint itemID) external view returns (int price, address owner, int userID, bool paid){
-        return (items[itemID].price, items[itemID].owner, items[itemID].buyer, items[itemID].paid);
+    function getItem(uint itemID) external view returns (int price, address owner, string memory buyer, bool paid, string memory title, string memory description){
+        return (items[itemID].price, items[itemID].owner, items[itemID].buyer, items[itemID].paid, items[itemID].title, items[itemID].description);
     }
 
-    function pay(uint itemID, int buyer) public payable {
-        uint256 timestamp = block.timestamp;
+    function pay(uint itemID, string memory buyer) public payable {
+        //uint256 timestamp = block.timestamp;
         
         emit debug(msg.sender);
         if(msg.value >= uint(items[itemID].price)) {
@@ -36,17 +38,23 @@ contract SimpleStorage {
         }
     }
 
-    function getAllItems() external view  returns(int[] memory ids, address[] memory owners, int[] memory buyers, bool[] memory payed){
+    function getAllItems() external view  returns(int[] memory ids, address[] memory owners, bool[] memory payed, int[] memory prices, string[] memory buyers, string[] memory titles, string[] memory descriptions){
         uint lenght = itemsArray.length;
-        address[] memory owenrs_ = new address[](lenght);
-        int[] memory buyers_ = new int[](lenght);
+        address[] memory owners_ = new address[](lenght);
+        string[] memory buyers_ = new string[](lenght);
         bool[] memory payed_ = new bool[](lenght);
+        int[] memory prices_ = new int[](lenght);
+        string[] memory titles_ = new string[](lenght);
+        string[] memory descriptions_ = new string[](lenght);
         for(uint256 i=0; i < itemsArray.length; i++) {
             item  memory item_ = items[i];
-            owenrs_[i] = item_.owner;
+            owners_[i] = item_.owner;
             buyers_[i] = item_.buyer;
             payed_[i] = (item_.paid);
+            prices_[i] = (item_.price);
+            titles_[i] = (item_.title);
+            descriptions_[i] = item_.description;
         }
-        return (itemsArray, owenrs_, buyers_, payed_);
+        return (itemsArray, owners, payed_, prices_ , buyers_, titles_, descriptions_);
     }
 }
